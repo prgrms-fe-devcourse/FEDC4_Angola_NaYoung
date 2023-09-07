d519c9ed2561711ecd0fee289cb9a3aee49c4ed6
박민우 <alsd20177@gmail.com>
2023-09-08 03:25:42 +0900

    feat: #10 - 알림 조회, 읽음 처리, 생성 API hook 구현

--- src/apis/comment.ts
+++ src/apis/comment.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import useAxiosInstance from './instance';
 
 export const CREATE_COMMENT_KEY = 'createComment';
@@ -49,6 +49,74 @@ export const useFetchDeleteComment = () => {
 	return {
 		data,
 		createComment: mutate,
+		isSuccess,
+		isLoading,
+	};
+};
+
+export const useFetchGetNotifications = () => {
+	const { authInstance } = useAxiosInstance();
+
+	const fetcher = () => {
+		authInstance.get('/notifications');
+	};
+
+	const { data, isSuccess, isLoading } = useQuery(
+		GET_NOTIFICATIONS_KEY,
+		fetcher,
+		// {
+		//   select: (data) => {
+		//     return data.data;
+		//   }
+		// }
+	);
+
+	return {
+		data,
+		isSuccess,
+		isLoading,
+	};
+};
+
+export const useFetchReadNotifications = () => {
+	const { authInstance } = useAxiosInstance();
+
+	const fetcher = () => authInstance.put('/notifications/seen');
+
+	const { data, isSuccess, mutate, isLoading } = useMutation(
+		READ_NOTIFICATIONS_KEY,
+		fetcher,
+	);
+
+	return {
+		data,
+		readNotifications: mutate,
+		isSuccess,
+		isLoading,
+	};
+};
+
+interface CreateNotificationsRequestBody {
+	notificationType: 'COMMENT' | 'FOLLOW' | 'LIKE' | 'MESSAGE';
+	notificationTypeId: string;
+	userId: string;
+	postId: string | null;
+}
+
+export const useFetchCreateNotifications = () => {
+	const { authInstance } = useAxiosInstance();
+
+	const fetcher = (body: CreateNotificationsRequestBody) =>
+		authInstance.post('/notifications/create', body);
+
+	const { data, isSuccess, mutate, isLoading } = useMutation(
+		CREATE_NOTIFICATIONS_KEY,
+		fetcher,
+	);
+
+	return {
+		data,
+		createNotifications: mutate,
 		isSuccess,
 		isLoading,
 	};
