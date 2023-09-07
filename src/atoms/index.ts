3056af7f3d90e8f6ea5379d6ed5a721321f54913
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-07 17:09:26 +0900

    feat: #2 - atom token defaultValue 수정

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -3,13 +3,13 @@ import { atom, selector } from 'recoil';
 
 const authToken = atom<string | null>({
 	key: 'authToken',
-	default: null,
+	default: tokenStorage.getItem(),
 });
 
 export const authTokenState = selector({
 	key: 'autoTokenState',
 	get: ({ get }) => {
-		return get(authToken) || tokenStorage.getItem();
+		return get(authToken);
 	},
 	set: ({ set }, newToken) => {
 		set(authToken, newToken);
