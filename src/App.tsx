bc94921a165175795a41f6784f06b75b89e3f732
JiYoon <angella990825@gmail.com>
2023-09-08 14:13:13 +0900

    feat: #5 - Test 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
+import Test from './components/Test';
 
 const queryClient = new QueryClient();
 
@@ -7,7 +8,7 @@ const App = () => {
 	return (
 		<RecoilRoot>
 			<QueryClientProvider client={queryClient}>
-				<div>test</div>
+				<Test />
 			</QueryClientProvider>
 		</RecoilRoot>
 	);
