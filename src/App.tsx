480f52376829517c6d677e1784b4c719aa1d7ff6
yeongjun Kim <kyjun519@gmail.com>
2023-09-08 18:06:24 +0900

    feat: #7 - 테스트 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
-import FetchTest from './components/FetchTest';
-import FollowTest from './components/FetchTest/FollowTest';
-import SearchTest from './components/FetchTest/SearchTest';
 
 const queryClient = new QueryClient();
 
 const App = () => {
 	return (
 		<RecoilRoot>
-			<QueryClientProvider client={queryClient}>
-				<FetchTest />
-				<SearchTest />
-				<FollowTest />
-			</QueryClientProvider>
+			<QueryClientProvider client={queryClient}></QueryClientProvider>
 		</RecoilRoot>
 	);
 };
