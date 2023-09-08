29e42484bf62b057965f7123c3e10c3cbf4c0241
hayamaster <dnfxmfkdbzos@naver.com>
2023-09-08 15:07:03 +0900

    feat: #6 - useFetchPost, useFetchCreatePost, useFetchDeletePost 함수 구현

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { Post } from '@/types';
 import { AxiosError, AxiosResponse } from 'axios';
 import useAxiosInstance from './instance';
@@ -12,9 +12,7 @@ export const useFetchAllPosts = async () => {
 	const { data, isError, isLoading, isSuccess } = useQuery<
 		AxiosResponse<Post[]>,
 		AxiosError
-	>('allPosts', () => baseInstance.get(path), {
-		staleTime: 5000,
-	});
+	>('allPosts', () => baseInstance.get(path));
 	return {
 		allPostsData: data?.data,
 		allPostsSuccess: isSuccess,
@@ -30,9 +28,7 @@ export const useFetchUserPosts = async (authorId: number) => {
 	const { data, isError, isLoading, isSuccess } = useQuery<
 		AxiosResponse<Post[]>,
 		AxiosError
-	>('userPosts', () => baseInstance.get(path), {
-		staleTime: 5000,
-	});
+	>('userPosts', () => baseInstance.get(path));
 	return {
 		userPostsData: data?.data,
 		userPostsError: isError,
@@ -41,53 +37,60 @@ export const useFetchUserPosts = async (authorId: number) => {
 	};
 };
 
-// export const useFetchUserPosts = async (authorId: number) => {
-// 	const { baseInstance } = useAxiosInstance();
-// 	const path = `/posts/author/${authorId}`;
-// 	const res = useQuery('fetchUserPosts', () => baseInstance.get<Post[]>(path), {
-// 		staleTime: 5000,
-// 	});
+export const useFetchPost = async (postId: number) => {
+	const { baseInstance } = useAxiosInstance();
+	const path = `/posts/${postId}`;
 
-// 	return res;
-// };
+	const { data, isLoading, isSuccess, isError } = useQuery<
+		AxiosResponse<Post>,
+		AxiosError
+	>('post', () => baseInstance.get(path));
+	return {
+		postData: data?.data,
+		postLoading: isLoading,
+		postSuccess: isSuccess,
+		postError: isError,
+	};
+};
 
-// export const useFetchPost = async (postId: number) => {
-// 	const { baseInstance } = useAxiosInstance();
-// 	const path = `/posts/${postId}}`;
+export const useFetchCreatePost = async () => {
+	const { authInstance } = useAxiosInstance();
+	const path = `/posts/create`;
 
-// 	try {
-// 		const response = await baseInstance.get<Post>(path);
-// 		return response.data;
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// };
+	const { mutate, isLoading, isSuccess, isError } = useMutation(
+		'createPost',
+		(title: string) => {
+			const formData = new FormData();
+			formData.append('title', title);
+			formData.append('image', '');
+			formData.append('channelId', CHANNEL_ID);
+			return authInstance.post(path, formData);
+		},
+	);
+	return {
+		createPost: mutate,
+		createLoading: isLoading,
+		createSuccess: isSuccess,
+		createError: isError,
+	};
+};
 
-// export const useFetchCreatePost = async (title: string): Promise<void> => {
-// 	const { authInstance } = useAxiosInstance();
-// 	const path = `/posts/create`;
+interface deletePostRequestBody {
+	id: string;
+}
 
-// 	const formData = new FormData();
-// 	formData.append('title', title);
-// 	formData.append('image', '');
-// 	formData.append('channelId', CHANNEL_ID);
+export const useFetchDeletePost = async () => {
+	const { authInstance } = useAxiosInstance();
+	const path = `/posts/delete`;
 
-// 	try {
-// 		await authInstance.post(path, formData);
-// 		console.log('Creating Post Successes');
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// };
-
-// export const useFetchDeletePost = async (id: string): Promise<void> => {
-// 	const { authInstance } = useAxiosInstance();
-// 	const path = `/posts/delete`;
-
-// 	try {
-// 		await authInstance.delete(path, { data: { id } });
-// 		console.log('Deleting Post Successes');
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// };
+	const { mutate, isLoading, isSuccess, isError } = useMutation<
+		AxiosError,
+		deletePostRequestBody
+	>('deletePost', (body) => authInstance.delete(path, { data: body }));
+	return {
+		deletePost: mutate,
+		deletePostLoading: isLoading,
+		deletePostSuccess: isSuccess,
+		deletePostError: isError,
+	};
+};
