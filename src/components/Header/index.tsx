f11bdd50619485917be9dac3a41c993c5a664c91
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-09-09 17:21:34 +0900

    feat: #21 - Header 컴포넌트 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,152 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { getPathname, getTitle } from './utils';
+
+const Header = () => {
+	const INITIAL_KEYWORD =
+		getPathname(2) === 'post' ? '전체 포스트' : '전체 유저';
+
+	const location = useLocation();
+	const navigate = useNavigate();
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const [selectValue, setSelectValue] = useState<string>('follower');
+	const [keyword, setKeyword] = useState(INITIAL_KEYWORD);
+	const [tabValue, setTabValue] = useState('post');
+	const title = getTitle(location.pathname);
+
+	const onChangeSelect = (value: string) => {
+		searchParams.set('sort', value);
+		setSearchParams(searchParams);
+	};
+
+	const onClickTabBar = (value: string) => {
+		const checkPath = getPathname(2);
+
+		if (value === 'user') {
+			if (checkPath === 'user') {
+				return;
+			}
+
+			navigate('/search/user?sort=follower');
+		}
+
+		if (value === 'post') {
+			if (checkPath === 'post') {
+				return;
+			}
+
+			navigate('/search/post?sort=recent');
+		}
+	};
+
+	useEffect(() => {
+		const currentSort = searchParams.get('sort');
+		const currentKeyword = searchParams.get('keyword');
+
+		if (currentSort) {
+			setSelectValue(currentSort);
+		}
+
+		if (currentKeyword) {
+			setKeyword(currentKeyword);
+		}
+	}, [searchParams]);
+
+	useEffect(() => {
+		setTabValue(getPathname(2));
+
+		const currentKeyword = searchParams.get('keyword');
+
+		if (!currentKeyword) {
+			setKeyword(INITIAL_KEYWORD);
+		}
+	}, [tabValue, location.pathname, INITIAL_KEYWORD, searchParams]);
+
+	return (
+		<Container>
+			<SortSelect
+				id="orderSelect"
+				name="order"
+				value={selectValue}
+				onChange={(e) => {
+					onChangeSelect(e.target.value);
+				}}>
+				{tabValue === 'user' ? (
+					<>
+						<option value="follower">팔로워순</option>
+						<option value="like">좋아요순</option>
+					</>
+				) : (
+					<>
+						<option value="recent">최신순</option>
+						<option value="like">좋아요순</option>
+					</>
+				)}
+			</SortSelect>
+
+			<Title>
+				<Keyword>
+					{keyword && getPathname(1) === 'search' ? keyword : null}
+				</Keyword>
+				{title}
+			</Title>
+
+			<TabBar>
+				<TabBarList
+					className={tabValue === 'user' ? 'bold' : ''}
+					onClick={() => onClickTabBar('user')}>
+					유저
+				</TabBarList>
+				<TabBarList
+					className={tabValue === 'post' ? 'bold' : ''}
+					onClick={() => onClickTabBar('post')}>
+					포스트
+				</TabBarList>
+			</TabBar>
+		</Container>
+	);
+};
+
+export default Header;
+
+const Container = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 80%;
+	border: 1px solid black;
+	border-radius: 20px 20px 0 0;
+	padding: 0 10px;
+`;
+const SortSelect = styled.select`
+	border-radius: 20px;
+	cursor: pointer;
+	padding: 5px 10px;
+`;
+
+const Title = styled.div``;
+
+const Keyword = styled.span`
+	font-weight: 600;
+`;
+
+const TabBar = styled.ul`
+	display: flex;
+	list-style: none;
+`;
+
+const TabBarList = styled.li`
+	padding-left: 10px;
+	cursor: pointer;
+
+	&:first-of-type {
+		padding-right: 10px;
+		border-right: 1px solid black;
+	}
+
+	&.bold {
+		font-weight: 800;
+	}
+`;
---
+++ src/components/Header/utils/index.ts
@@ -0,0 +1,9 @@
+import { TITLE } from '../constants';
+
+export const getPathname = (segment: number) => {
+	return location.pathname.split('/')[segment];
+};
+
+export const getTitle = (pathname: string) => {
+	return pathname.startsWith(`/${getPathname(1)}`) && TITLE[getPathname(1)];
+};
