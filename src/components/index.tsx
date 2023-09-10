4814d4f03bb14365fe7812c5b4ecd5c887148136
박민우 <alsd20177@gmail.com>
2023-09-10 16:37:49 +0900

    feat: #34 - Spinner의 위치를 props로 설정해줄 수 있도록 수정

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,19 +1,25 @@
+import { CSSProperties } from 'react';
 import { HashLoader } from 'react-spinners';
 
+const DEFAULT_POSITION_STYLE: Record<string, string> = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+};
 interface SpinnerProps {
-  color: string;
-  size: number;
+  color?: string;
+  size?: number;
+  spinnerPositionStyle?: CSSProperties;
 }
 
-const Spinner = ({ color = '#86B7FF', size = 100 }: SpinnerProps) => {
+const Spinner = ({
+  color = '#86B7FF',
+  size = 100,
+  spinnerPositionStyle = DEFAULT_POSITION_STYLE,
+}: SpinnerProps) => {
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-      }}>
+    <div style={{ ...spinnerPositionStyle }}>
       <HashLoader
         color={color}
         size={size}
