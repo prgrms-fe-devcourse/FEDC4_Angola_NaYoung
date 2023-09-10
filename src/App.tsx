18879a5d4abe12e568eec8a6dbcf36f21f6ae4ac
hayamaster <dnfxmfkdbzos@naver.com>
2023-09-10 21:45:22 +0900

    chore: #27 - test를 위해 App.tsx와 LogIn파일 일시적수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import FetchTest from '@components/FetchTest';
+// import FetchTest from './components/FetchTest';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <RecoilRoot>
-        <QueryClientProvider client={queryClient}>
-          <FetchTest />
-        </QueryClientProvider>
-      </RecoilRoot>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<RecoilRoot>
+				<QueryClientProvider client={queryClient}>
+					{/* <FetchTest /> */}
+					<Routes>
+						<Route
+							key={'login'}
+							path={'/login'}
+							element={React.createElement(LogIn, {})}></Route>
+						<Route
+							key={'signup'}
+							path={'/'}
+							element={React.createElement(SignUp, {})}></Route>
+					</Routes>
+				</QueryClientProvider>
+			</RecoilRoot>
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- src/apis/post.ts
+++ src/apis/post.ts
@@ -1,100 +1,100 @@
 import { useMutation, useQuery } from 'react-query';
+import { Post } from '@/types';
 import { AxiosError, AxiosResponse } from 'axios';
-import { Post } from '@type/index';
 import useAxiosInstance from './instance';
 
 const CHANNEL_ID = '64fab06721f5351a7dd21a66';
 
 export const useFetchAllPosts = () => {
-  const { baseInstance } = useAxiosInstance();
-  const path = `/posts/channel/${CHANNEL_ID}`;
+	const { baseInstance } = useAxiosInstance();
+	const path = `/posts/channel/${CHANNEL_ID}`;
 
-  const { data, isError, isLoading, isSuccess } = useQuery<
-    AxiosResponse<Post[]>,
-    AxiosError
-  >('allPosts', () => baseInstance.get(path));
-  return {
-    allPostsData: data?.data,
-    allPostsSuccess: isSuccess,
-    allPostsError: isError,
-    allPostsLoading: isLoading,
-  };
+	const { data, isError, isLoading, isSuccess } = useQuery<
+		AxiosResponse<Post[]>,
+		AxiosError
+	>('allPosts', () => baseInstance.get(path));
+	return {
+		allPostsData: data?.data,
+		allPostsSuccess: isSuccess,
+		allPostsError: isError,
+		allPostsLoading: isLoading,
+	};
 };
 
 export const useFetchUserPosts = (authorId: string) => {
-  const { baseInstance } = useAxiosInstance();
-  const path = `/posts/author/${authorId}`;
+	const { baseInstance } = useAxiosInstance();
+	const path = `/posts/author/${authorId}`;
 
-  const { data, isError, isLoading, isSuccess } = useQuery<
-    AxiosResponse<Post[]>,
-    AxiosError
-  >('userPosts', () => baseInstance.get(path));
-  return {
-    userPostsData: data?.data,
-    userPostsError: isError,
-    userPostsLoading: isLoading,
-    userPostsSuccess: isSuccess,
-  };
+	const { data, isError, isLoading, isSuccess } = useQuery<
+		AxiosResponse<Post[]>,
+		AxiosError
+	>('userPosts', () => baseInstance.get(path));
+	return {
+		userPostsData: data?.data,
+		userPostsError: isError,
+		userPostsLoading: isLoading,
+		userPostsSuccess: isSuccess,
+	};
 };
 
 export const useFetchPost = (postId: string) => {
-  const { baseInstance } = useAxiosInstance();
-  const path = `/posts/${postId}`;
+	const { baseInstance } = useAxiosInstance();
+	const path = `/posts/${postId}`;
 
-  const { data, isLoading, isSuccess, isError } = useQuery<
-    AxiosResponse<Post>,
-    AxiosError
-  >('post', () => baseInstance.get(path));
-  return {
-    postData: data?.data as Post,
-    postLoading: isLoading,
-    postSuccess: isSuccess,
-    postError: isError,
-  };
+	const { data, isLoading, isSuccess, isError } = useQuery<
+		AxiosResponse<Post>,
+		AxiosError
+	>('post', () => baseInstance.get(path));
+	return {
+		postData: data?.data as Post,
+		postLoading: isLoading,
+		postSuccess: isSuccess,
+		postError: isError,
+	};
 };
 
 interface CreatePostRequestBody {
-  title: string;
+	title: string;
 }
 
 export const useFetchCreatePost = () => {
-  const { authInstance } = useAxiosInstance();
-  const path = `/posts/create`;
+	const { authInstance } = useAxiosInstance();
+	const path = `/posts/create`;
 
-  const { mutate, isLoading, isSuccess, isError } = useMutation(
-    'createPostMutation',
-    (body: CreatePostRequestBody) => {
-      const formData = new FormData();
-      formData.append('title', body.title);
-      formData.append('image', '');
-      formData.append('channelId', CHANNEL_ID);
-      return authInstance.post(path, formData);
-    },
-  );
-  return {
-    createPostMutate: mutate,
-    createPostLoading: isLoading,
-    createPostSuccess: isSuccess,
-    createPostError: isError,
-  };
+	const { mutate, isLoading, isSuccess, isError } = useMutation(
+		'createPostMutation',
+		(body: CreatePostRequestBody) => {
+			const formData = new FormData();
+			formData.append('title', body.title);
+			formData.append('image', '');
+			formData.append('channelId', CHANNEL_ID);
+			return authInstance.post(path, formData);
+		},
+	);
+	return {
+		createPostMutate: mutate,
+		createPostLoading: isLoading,
+		createPostSuccess: isSuccess,
+		createPostError: isError,
+	};
 };
 
 interface DeletePostRequestBody {
-  id: string;
+	id: string;
 }
 
 export const useFetchDeletePost = () => {
-  const { authInstance } = useAxiosInstance();
-  const path = `/posts/delete`;
+	const { authInstance } = useAxiosInstance();
+	const path = `/posts/delete`;
 
-  const { mutate, isLoading, isSuccess, isError } = useMutation(
-    'deletePostMutation',
-    (body: DeletePostRequestBody) => authInstance.delete(path, { data: body }),
-  );
-  return {
-    deletePostMutate: mutate,
-    deletePostLoading: isLoading,
-    deletePostSuccess: isSuccess,
-    deletePostError: isError,
-  };
+	const { mutate, isLoading, isSuccess, isError } = useMutation(
+		'deletePostMutation',
+		(body: DeletePostRequestBody) => authInstance.delete(path, { data: body }),
+	);
+	return {
+		deletePostMutate: mutate,
+		deletePostLoading: isLoading,
+		deletePostSuccess: isSuccess,
+		deletePostError: isError,
+	};
 };
