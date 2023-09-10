cbb37294b07631bdc18198617132d535ea1796f1
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-10 17:05:39 +0900

    feat: #29 - Main 경로 수정

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import useCurrentPage from '@/hooks/useCurrentPage';
-import { redirects, routes } from '@/routes';
+import { redirects, routes } from '@routes';
+import useCurrentPage from '@hooks/useCurrentPage';
 
 // 임시 컴포넌트, Header 합칠 때 삭제해야 함
 const Header = ({ title }: { title: string }) => {
-	return <h1>{title}</h1>;
+  return <h1>{title}</h1>;
 };
 
 const Main = () => {
-	const { title, params, search } = useCurrentPage();
-	return (
-		<>
-			<Header title={title} />
-			<Routes>
-				{routes.map(({ path, title, component }) => (
-					<Route
-						key={title}
-						path={path}
-						element={React.createElement(component, {
-							...params,
-							...search,
-						})}></Route>
-				))}
-				{redirects.map(({ from, to }) => (
-					<Route
-						key={from + to}
-						path={from}
-						element={<Navigate to={to} />}
-					/>
-				))}
-			</Routes>
-		</>
-	);
+  const { title, params, search } = useCurrentPage();
+  return (
+    <>
+      <Header title={title} />
+      <Routes>
+        {routes.map(({ path, title, component }) => (
+          <Route
+            key={title}
+            path={path}
+            element={React.createElement(component, {
+              ...params,
+              ...search,
+            })}></Route>
+        ))}
+        {redirects.map(({ from, to }) => (
+          <Route
+            key={from + to}
+            path={from}
+            element={<Navigate to={to} />}
+          />
+        ))}
+      </Routes>
+    </>
+  );
 };
 
 export default Main;
