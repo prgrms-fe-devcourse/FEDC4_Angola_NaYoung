deaf23c9077e273c9d45f0c7766b90cda3bbe1d4
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-10 17:03:37 +0900

    feat: #29 - useCurrentPage 경로 수정

--- src/hooks/useCurrentPage.ts
+++ src/hooks/useCurrentPage.ts
@@ -1,47 +1,47 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useLocation, useMatch } from 'react-router-dom';
-import { routes } from '@/routes';
-import { parseQueryString } from '@/utils/parseQueryString';
+import { routes } from '@routes';
+import { parseQueryString } from '@utils/parseQueryString';
 
 interface SearchParams {
-	show?: 'true';
-	voted?: 'true';
-	keyword?: string;
-	sort?: string;
+  show?: 'true';
+  voted?: 'true';
+  keyword?: string;
+  sort?: string;
 }
 
 interface Params {
-	target?: 'user' | 'post';
-	userId?: string;
-	postId?: string;
+  target?: 'user' | 'post';
+  userId?: string;
+  postId?: string;
 }
 
 interface CurrentPage {
-	title: string;
-	params: Params;
-	search: SearchParams;
+  title: string;
+  params: Params;
+  search: SearchParams;
 }
 
 const useCurrentPage = (): CurrentPage => {
-	const location = useLocation();
-	const searchParams = parseQueryString(location.search);
+  const location = useLocation();
+  const searchParams = parseQueryString(location.search);
 
-	let result: CurrentPage = {
-		title: 'not found',
-		params: {},
-		search: {},
-	};
-	for (const route of routes) {
-		const match = useMatch(route.path);
-		if (match) {
-			result = {
-				title: route.title,
-				params: match.params,
-				search: searchParams,
-			};
-		}
-	}
-	return result;
+  let result: CurrentPage = {
+    title: 'not found',
+    params: {},
+    search: {},
+  };
+  for (const route of routes) {
+    const match = useMatch(route.path);
+    if (match) {
+      result = {
+        title: route.title,
+        params: match.params,
+        search: searchParams,
+      };
+    }
+  }
+  return result;
 };
 
 export default useCurrentPage;
