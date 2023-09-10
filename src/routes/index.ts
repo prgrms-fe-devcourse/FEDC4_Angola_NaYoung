4fcaacc84911eae476723ff39335e5d26fe63049
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-10 17:03:16 +0900

    feat: #29 - routes 경로 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,27 +1,29 @@
 import {
-	CreatePost,
-	Home,
-	Login,
-	MyPage,
-	Post,
-	Search,
-	Signup,
-	User,
-} from '@/pages';
+  CreatePost,
+  Home,
+  Login,
+  MyPage,
+  Post,
+  Search,
+  Signup,
+  User,
+} from '@pages';
+
+// todo: page 넣을 때 수정
 
 export const routes = [
-	{ path: '/', title: 'home', component: Home },
-	{ path: '/search/:target', title: 'search', component: Search },
-	{ path: '/signup', title: 'signup', component: Signup },
-	{ path: '/login', title: 'login', component: Login },
-	{ path: '/user/:userId', title: 'user', component: User },
-	{ path: '/post/:postId', title: 'post', component: Post },
-	{ path: '/create-post', title: 'createPost', component: CreatePost },
-	{ path: '/mypage', title: 'myPage', component: MyPage },
+  { path: '/', title: 'home', component: Home },
+  { path: '/search/:target', title: 'search', component: Search },
+  { path: '/signup', title: 'signup', component: Signup },
+  { path: '/login', title: 'login', component: Login },
+  { path: '/user/:userId', title: 'user', component: User },
+  { path: '/post/:postId', title: 'post', component: Post },
+  { path: '/create-post', title: 'createPost', component: CreatePost },
+  { path: '/mypage', title: 'myPage', component: MyPage },
 ];
 
 export const redirects = [
-	{ from: '/post', to: '/' },
-	{ from: '/user', to: '/' },
-	{ from: '/search', to: '/search/post' },
+  { from: '/post', to: '/' },
+  { from: '/user', to: '/' },
+  { from: '/search', to: '/search/post' },
 ];
