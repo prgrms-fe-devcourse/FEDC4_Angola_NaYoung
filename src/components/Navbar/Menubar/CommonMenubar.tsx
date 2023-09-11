2ae8836b5cdc20591c36a5a8578d14e6bcde25c9
JiYoon <angella990825@gmail.com>
2023-09-11 11:01:31 +0900

    refactor: #25 - useCurrentPage로 SearchBar 조건부 렌더링

--- src/components/Navbar/Menubar/CommonMenubar.tsx
+++ src/components/Navbar/Menubar/CommonMenubar.tsx
@@ -1,25 +1,11 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getPathname } from '@utils/index';
 import LinkButton from '../LinkButton';
 import SearchBar from '../SearchBar';
+import useCurrentPage from '../hooks/useCurrentPage';
 
 const SEARCH = 'search';
 
 const CommonMenubar = () => {
-  const navigate = useNavigate();
-  const isSearchPage = getPathname(1) === SEARCH;
-  const [searchBarVisible, setSearchBarVisible] = useState(isSearchPage);
-
-  const onClickSearchButton = () => {
-    if (!isSearchPage) {
-      setSearchBarVisible(true);
-      navigate('/search');
-    }
-  };
-
-  const renderSearchBar = isSearchPage && searchBarVisible;
-
+  const { name } = useCurrentPage();
   return (
     <>
       <LinkButton
@@ -27,10 +13,11 @@ const CommonMenubar = () => {
         style={{ backgroundColor: 'orange', fontSize: 20, fontWeight: 'bold' }}>
         홈
       </LinkButton>
-      {!renderSearchBar && (
-        <button onClick={onClickSearchButton}>검색버튼</button>
+      {name === SEARCH ? (
+        <SearchBar />
+      ) : (
+        <LinkButton to="/search">검색</LinkButton>
       )}
-      {renderSearchBar && <SearchBar />}
     </>
   );
 };
