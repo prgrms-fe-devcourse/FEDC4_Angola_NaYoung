803f68f41f3c018100f655903e7c23216b4ebc7a
박민우 <alsd20177@gmail.com>
2023-09-12 22:00:08 +0900

    feat: #43 - 포스트 작성 API와 연결, 로딩 시 Spinner 렌더링

--- src/pages/CreatePostPage/index.tsx
+++ src/pages/CreatePostPage/index.tsx
@@ -1,5 +1,8 @@
 import { ChangeEvent, FormEvent, KeyboardEvent, useState } from 'react';
 import styled from '@emotion/styled';
+import Spinner from '@components/Spinner';
+import { useFetchCreatePost } from '@apis/post';
+import { joinDataBySeparator } from '@utils/parseDataBySeparator';
 
 const CreatePostPage = () => {
   const [inputValues, setInputValues] = useState({
@@ -7,6 +10,9 @@ const CreatePostPage = () => {
     optionA: '',
     optionB: '',
   });
+
+  const { createPostMutate, isCreatePostLoading, isCreatePostSuccess } =
+    useFetchCreatePost();
 
   const handleChangeInputValues = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
@@ -21,7 +27,7 @@ const CreatePostPage = () => {
     inputValues.optionA.length > 0 &&
     inputValues.optionB.length > 0;
 
-  const handleEnterKeySubmit = (e: KeyboardEvent<HTMLInputElement>) => {
+  const handlePreventEnterKeySubmit = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
     }
@@ -29,52 +35,73 @@ const CreatePostPage = () => {
 
   const handleSubmitMakePost = (e: FormEvent) => {
     e.preventDefault();
-    console.log('전송합니다');
+
+    // 3개 input 창 모두 입력되어있을 때만 API 호출 가능
+    if (!isMakePostPossible) return;
+
+    const postTitle = joinDataBySeparator(
+      inputValues.title,
+      inputValues.optionA,
+      inputValues.optionB,
+    );
+
+    console.log(`postTitle을 ${postTitle}로 api에 넣어줄 것임`);
+
+    createPostMutate({ title: postTitle }); // 이후 isLoading, isSuccess 바뀔 것임
   };
 
   return (
-    <StyledForm onSubmit={handleSubmitMakePost}>
-      <TitleInput
-        id="title"
-        placeholder="한 줄 설명 쓰기"
-        value={inputValues.title}
-        maxLength={100}
-        onChange={handleChangeInputValues}
-        onKeyDown={handleEnterKeySubmit}
-      />
-
-      <ContentContainer>
-        <OptionContainer>
-          <label>A 항목</label>
-          <OptionInput
-            id="optionA"
-            value={inputValues.optionA}
+    <>
+      {isCreatePostLoading ? (
+        <Spinner
+          size={100}
+          color={'#111111'}
+        />
+      ) : (
+        <StyledForm onSubmit={handleSubmitMakePost}>
+          <TitleInput
+            id="title"
+            placeholder="한 줄 설명 쓰기"
+            value={inputValues.title}
             maxLength={100}
             onChange={handleChangeInputValues}
-            onKeyDown={handleEnterKeySubmit}
+            onKeyDown={handlePreventEnterKeySubmit}
           />
-        </OptionContainer>
 
-        <p>VS</p>
+          <ContentContainer>
+            <OptionContainer>
+              <label>A 항목</label>
+              <OptionInput
+                id="optionA"
+                value={inputValues.optionA}
+                maxLength={100}
+                onChange={handleChangeInputValues}
+                onKeyDown={handlePreventEnterKeySubmit}
+              />
+            </OptionContainer>
 
-        <OptionContainer>
-          <label>B 항목</label>
-          <OptionInput
-            id="optionB"
-            value={inputValues.optionB}
-            maxLength={100}
-            onChange={handleChangeInputValues}
-            onKeyDown={handleEnterKeySubmit}
-          />
-        </OptionContainer>
-      </ContentContainer>
+            <p>VS</p>
 
-      <StyledButton
-        type="submit"
-        disabled={!isMakePostPossible}>
-        작성 완료하기
-      </StyledButton>
-    </StyledForm>
+            <OptionContainer>
+              <label>B 항목</label>
+              <OptionInput
+                id="optionB"
+                value={inputValues.optionB}
+                maxLength={100}
+                onChange={handleChangeInputValues}
+                onKeyDown={handlePreventEnterKeySubmit}
+              />
+            </OptionContainer>
+          </ContentContainer>
+
+          <StyledButton
+            type="submit"
+            disabled={!isMakePostPossible}>
+            작성 완료하기
+          </StyledButton>
+        </StyledForm>
+      )}
+    </>
   );
 };
 
