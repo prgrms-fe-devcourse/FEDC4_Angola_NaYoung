01ff754a5595b794ba4012ac51dac96726c25765
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-12 15:49:11 +0900

    feat: #49 - join, split 함수 추가 및 파일들 다시 이동

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,12 +2,9 @@ export const getPathname = (segment: number) => {
   return location.pathname.split('/')[segment];
 };
 
-export const parseQueryString = (queryString: string) => {
-  return queryString
-    .slice(1)
-    .split('&')
-    .reduce((obj, query) => {
-      const [key, value] = query.split('=');
-      return { ...obj, [key]: value };
-    }, {});
-};
+export { parseQueryString } from '@utils/parseQueryString';
+export {
+  joinDataBySeparator,
+  splitCommentBySeparator,
+  splitPostBySeparator,
+} from '@utils/parseDataBySeparator';
