a35508b284c4c0c32f65f43ecd456183fc88fafb
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-12 22:42:55 +0900

    feat: #46 - postPage 및 컴포넌트 테스트 추가

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,22 +1,74 @@
-import { useFetchPost } from '@apis/post';
+import styled from '@emotion/styled';
+import { splitPostBySeparator } from '@utils/parseDataBySeparator';
 
 interface PostProps {
   postId: string;
+  postTitle: string;
+  authorName: string;
+  authorId: string;
+  voteValue?: string;
+  onVote?: (value: string) => void;
 }
 
-const Post = ({ postId }: PostProps) => {
-  const { postData, isPostError, isPostLoading, isPostSuccess } =
-    useFetchPost(postId);
+const Post = ({
+  postId,
+  postTitle,
+  authorName,
+  authorId,
+  voteValue,
+  onVote,
+}: PostProps) => {
+  const { a, b, title } = splitPostBySeparator(postTitle);
+  const handleClickVoteButton = (value: string) => {
+    onVote && onVote(value);
+  };
   return (
-    <div>
-      <h1>Post</h1>
-      <h2>Author: {postData?.author.fullName}</h2>
-      <h2>Title: {postData?.title}</h2>
-      <div>error: {isPostError ? 'error' : 'none'}</div>
-      <div>loading: {isPostLoading ? 'loading' : 'done'}</div>
-      <div>success: {isPostSuccess ? 'success' : 'fail'}</div>
-    </div>
+    <PostContainer>
+      <h1>Post 컴포넌트</h1>
+      <h2>Post: {postId}</h2>
+      <h2>Author: {authorName}</h2>
+      <h2>AuthorId: {authorId}</h2>
+      <h2>Title: {title}</h2>
+      <VoteButtonContainer>
+        <VoteButton
+          className={voteValue === 'a' ? 'active' : ''}
+          onClick={() => handleClickVoteButton('a')}>
+          {a}
+        </VoteButton>
+        <VoteButton
+          className={voteValue === 'b' ? 'active' : ''}
+          onClick={() => handleClickVoteButton('b')}>
+          {b}
+        </VoteButton>
+      </VoteButtonContainer>
+    </PostContainer>
   );
 };
 
 export default Post;
+
+const PostContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  > h1 {
+    font-size: 20px;
+  }
+`;
+
+const VoteButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const VoteButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  &.active {
+    background-color: orangered;
+  }
+`;
--- src/pages/tempPages.tsx
+++ src/pages/tempPages.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
 import Post from '@components/Post';
+import { useFetchPost } from '@apis/post';
 
 export const CreatePostPage = () => {
   return <div>CreatePostPage</div>;
@@ -16,12 +19,67 @@ export const MyPage = () => {
 export interface PostPageProps {
   postId?: string;
   show?: 'true';
-  voted?: 'a' | 'b';
+  voted?: string;
 }
 export const PostPage = ({ postId = '', show, voted }: PostPageProps) => {
+  const { postData, isPostError, isPostLoading, isPostSuccess } =
+    useFetchPost(postId);
+  const [votedValue, setVotedValue] = useState('');
   return (
     <div>
-      <Post postId={postId} />
+      {postData && (
+        <Post
+          postId={postId}
+          authorName={postData.author.fullName}
+          authorId={postData.author._id}
+          postTitle={postData.title}
+          voteValue={votedValue}
+          onVote={(value: string) => setVotedValue(value)}
+        />
+      )}
+      <hr />
+
+      <CommentContainer>
+        <h1>댓글창</h1>
+        <SelectContainer>
+          <SelectBox
+            onClick={() => setVotedValue('a')}
+            className={votedValue === 'a' ? 'active' : ''}>
+            button A
+          </SelectBox>
+          <SelectBox
+            onClick={() => setVotedValue('b')}
+            className={votedValue === 'b' ? 'active' : ''}>
+            button B
+          </SelectBox>
+        </SelectContainer>
+      </CommentContainer>
     </div>
   );
 };
+
+const CommentContainer = styled.div`
+  padding: 20px;
+  background-color: lightyellow;
+  > h1 {
+    font-size: 20px;
+  }
+`;
+const SelectContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+const SelectBox = styled.div`
+  width: 200px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: lightgray;
+  &.active {
+    background-color: pink;
+  }
+`;
