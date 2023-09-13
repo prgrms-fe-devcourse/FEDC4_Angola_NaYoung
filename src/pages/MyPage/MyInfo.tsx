6f4512658b07fef7fe439cf61a6ca59ee40837eb
JiYoon <109654823+JIY00N2@users.noreply.github.com>
2023-09-13 18:09:58 +0900

    feat: #38 - MyInfoProps 타입 이름 변경

--- src/pages/MyPage/MyInfo.tsx
+++ src/pages/MyPage/MyInfo.tsx
@@ -6,7 +6,7 @@ import { useFetchUpdateFullName, useFetchUpdatePassword } from '@apis/profile';
 import { checkFullNamePattern, checkPassWordPattern } from './utils';
 
 // TODO: return에서 isUpdateFullNameError 발생 시, 모달 보여주고 함수 실행시키고, 이전 값
-interface UserInfoProps {
+interface MyInfoProps {
   id: string;
   image: string;
   name: string;
@@ -22,7 +22,7 @@ const MyInfo = ({
   likes,
   followers,
   following,
-}: UserInfoProps) => {
+}: MyInfoProps) => {
   const navigate = useNavigate();
   const { updateFullNameMutate } = useFetchUpdateFullName();
   const [newFullName, setNewFullName] = useState(name);
