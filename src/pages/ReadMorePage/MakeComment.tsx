5ba8ba3e4f04413eb3548b5471bc58a2b5cb2066
hayamaster <dnfxmfkdbzos@naver.com>
2023-09-13 15:13:51 +0900

    refactor: #51 - MakeComment 컴포넌트 분리

--- src/pages/ReadMorePage/MakeComment.tsx
+++ src/pages/ReadMorePage/MakeComment.tsx
@@ -1,103 +1,101 @@
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 
-// const MakeComment = () => {
-//   const handleClickItemA = () => {
-//     votedValue === 'A' ? setVotedValue('') : setVotedValue('A');
-//   };
+interface MakeCommentProps {
+  votedValue: string;
+  handleClickItem: (value: string) => void;
+}
 
-//   const handleClickItemB = () => {
-//     votedValue === 'B' ? setVotedValue('') : setVotedValue('B');
-//   };
-//   return (
-//     <>
-//       <MakeCommentContainer>
-//         <ItemButtonsContainer>
-//           <ItemButtonA
-//             onClick={handleClickItemA}
-//             votedValue={votedValue}>
-//             A
-//           </ItemButtonA>
-//           <ItemButtonB
-//             onClick={handleClickItemB}
-//             votedValue={votedValue}>
-//             B
-//           </ItemButtonB>
-//         </ItemButtonsContainer>
-//         <Comment placeholder="의견 입력창"></Comment>
-//         <SubmitButton disabled={votedValue ? false : true}>
-//           <p>submit</p>
-//           <p>또는</p>
-//           <p>skip</p>
-//         </SubmitButton>
-//       </MakeCommentContainer>
-//     </>
-//   );
-// };
+const MakeComment = ({ votedValue, handleClickItem }: MakeCommentProps) => {
+  return (
+    <>
+      <MakeCommentContainer>
+        <ItemButtonsContainer>
+          <ItemButtonA
+            onClick={() => handleClickItem('A')}
+            votedValue={votedValue}>
+            A
+          </ItemButtonA>
+          <ItemButtonB
+            onClick={() => handleClickItem('B')}
+            votedValue={votedValue}>
+            B
+          </ItemButtonB>
+        </ItemButtonsContainer>
+        <Comment placeholder="의견 입력창"></Comment>
+        <SubmitButton disabled={votedValue ? false : true}>
+          <p>submit</p>
+          <p>또는</p>
+          <p>skip</p>
+        </SubmitButton>
+      </MakeCommentContainer>
+    </>
+  );
+};
 
-// export default MakeComment;
+export default MakeComment;
 
-// const MakeCommentContainer = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   padding: 1rem;
-//   border: 1px solid black;
-//   z-index: 100;
-//   gap: 1rem;
-// `;
+const MakeCommentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  border: 1px solid black;
+  z-index: 100;
+  gap: 1rem;
+`;
 
-// const ItemButtonsContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   border: 1px solid black;
-//   border-radius: 40px;
-//   overflow: hidden;
-//   width: 3rem;
-//   height: 6rem;
-// `;
+const ItemButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  border-radius: 40px;
+  overflow: hidden;
+  width: 3rem;
+  height: 6rem;
+`;
 
-// const ItemButtonA = styled.button<{ votedValue: string }>`
-//   width: 100%;
-//   height: 100%;
-//   border: none;
-//   background-color: white;
-//   font-size: 1.5rem;
-//   border-bottom: solid;
-//   cursor: pointer;
-//   background-color: ${(props) => (props.votedValue === 'A' ? 'pink' : 'none')};
+const ItemButtonA = styled.button<{ votedValue: string }>`
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: white;
+  font-size: 1.5rem;
+  border-bottom: solid;
+  cursor: pointer;
+  background-color: ${(props) => (props.votedValue === 'A' ? 'pink' : 'none')};
 
-//   &:hover {
-//     background-color: gray;
-//   }
-// `;
+  &:hover {
+    background-color: gray;
+  }
+`;
 
-// const ItemButtonB = styled.button<{ votedValue: string }>`
-//   width: 100%;
-//   height: 100%;
-//   border: none;
-//   background-color: white;
-//   font-size: 1.5rem;
-//   cursor: pointer;
-//   background-color: ${(props) => (props.votedValue === 'B' ? 'pink' : 'none')};
+const ItemButtonB = styled.button<{ votedValue: string }>`
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: white;
+  font-size: 1.5rem;
+  cursor: pointer;
+  background-color: ${(props) => (props.votedValue === 'B' ? 'pink' : 'none')};
 
-//   &:hover {
-//     background-color: gray;
-//   }
-// `;
+  &:hover {
+    background-color: gray;
+  }
+`;
 
-// const Comment = styled.input`
-//   border: 1px solid black;
-//   border-radius: 3rem;
-//   padding: 1rem;
-//   width: 80%;
-// `;
+const Comment = styled.input`
+  border: 1px solid black;
+  border-radius: 3rem;
+  padding: 1rem;
+  width: 80%;
+`;
 
-// const SubmitButton = styled.button`
-//   background-color: white;
-//   border: 1px solid black;
-//   border-radius: 2rem;
-//   cursor: pointer;
+const SubmitButton = styled.button`
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 2rem;
+  cursor: pointer;
 
-//   &:hover {
-//     background-color: gray;
-//   }
-// `;
+  &:hover {
+    background-color: gray;
+  }
+`;
--- src/pages/ReadMorePage/index.tsx
+++ src/pages/ReadMorePage/index.tsx
@@ -3,8 +3,7 @@ import { authInfoState } from '@atoms';
 import styled from '@emotion/styled';
 import { useRecoilValue } from 'recoil';
 import TestComponent from '@pages/ReadMorePage/TestComponent';
-
-// import MakeComment from "./MakeComment";
+import MakeComment from './MakeComment';
 
 // postData && ( // postData 가 있을 때만 Post 컴포넌트를 렌더링 해주세요!
 //   <Post
@@ -22,13 +21,6 @@ const ReadMorePage = () => {
 
   console.log(userId);
 
-  const handleClickItemA = () => {
-    votedValue === 'A' ? setVotedValue('') : setVotedValue('A');
-  };
-
-  const handleClickItemB = () => {
-    votedValue === 'B' ? setVotedValue('') : setVotedValue('B');
-  };
   const handleClickItem = (value: string) => {
     votedValue === value ? setVotedValue('') : setVotedValue(value);
   };
@@ -41,26 +33,10 @@ const ReadMorePage = () => {
           onVote={(value: string) => handleClickItem(value)}
         />
         <CommentsContainer>
-          <MakeCommentContainer>
-            <ItemButtonsContainer>
-              <ItemButtonA
-                onClick={handleClickItemA}
-                votedValue={votedValue}>
-                A
-              </ItemButtonA>
-              <ItemButtonB
-                onClick={handleClickItemB}
-                votedValue={votedValue}>
-                B
-              </ItemButtonB>
-            </ItemButtonsContainer>
-            <MakeComment placeholder="의견 입력창"></MakeComment>
-            <SubmitButton disabled={votedValue ? false : true}>
-              <p>submit</p>
-              <p>또는</p>
-              <p>skip</p>
-            </SubmitButton>
-          </MakeCommentContainer>
+          <MakeComment
+            votedValue={votedValue}
+            handleClickItem={handleClickItem}
+          />
           <CommentWrapper>
             <MakerName>작성자 이름</MakerName>
             <CommentSubWrapper>
@@ -94,74 +70,6 @@ const CommentsContainer = styled.div`
   border-radius: 45px;
   overflow: hidden;
 `;
-
-const MakeCommentContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding: 1rem;
-  border: 1px solid black;
-  z-index: 100;
-  gap: 1rem;
-`;
-
-const ItemButtonsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px solid black;
-  border-radius: 40px;
-  overflow: hidden;
-  width: 3rem;
-  height: 6rem;
-`;
-
-const ItemButtonA = styled.button<{ votedValue: string }>`
-  width: 100%;
-  height: 100%;
-  border: none;
-  background-color: white;
-  font-size: 1.5rem;
-  border-bottom: solid;
-  cursor: pointer;
-  background-color: ${(props) => (props.votedValue === 'A' ? 'pink' : 'none')};
-
-  &:hover {
-    background-color: gray;
-  }
-`;
-
-const ItemButtonB = styled.button<{ votedValue: string }>`
-  width: 100%;
-  height: 100%;
-  border: none;
-  background-color: white;
-  font-size: 1.5rem;
-  cursor: pointer;
-  background-color: ${(props) => (props.votedValue === 'B' ? 'pink' : 'none')};
-
-  &:hover {
-    background-color: gray;
-  }
-`;
-
-const MakeComment = styled.input`
-  border: 1px solid black;
-  border-radius: 3rem;
-  padding: 1rem;
-  width: 80%;
-`;
-
-const SubmitButton = styled.button`
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 2rem;
-  cursor: pointer;
-
-  &:hover {
-    background-color: gray;
-  }
-`;
-
-// here
 
 const CommentWrapper = styled.div`
   display: flex;
