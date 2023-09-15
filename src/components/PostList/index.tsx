9b37303e3f38ef873113c619611e42221105e7cd
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-09-16 00:42:02 +0900

    feat: #63 - 유저 좋아요 순 정렬을 삭제하고 레벨 순 정렬로 수정

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -25,9 +25,6 @@ const PostList = ({ keyword, sort }: PostListProps) => {
     searchPostsDataRefetch();
   }, [keyword, searchPostsDataRefetch]);
 
-  // useEffect(() => {
-  //   allPostsRefetch();
-  // }, [sort, allPostsRefetch]);
   return (
     <>
       <ul>
