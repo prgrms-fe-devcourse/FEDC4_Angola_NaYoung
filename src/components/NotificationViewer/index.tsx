61f4f8578d39ba810633732783b71e997ca036b6
박민우 <alsd20177@gmail.com>
2023-09-17 03:10:33 +0900

    feat: #71 - 알림 Viewer 바깥 부분 클릭 시, 알림 뷰어 닫히는 로직 추가

--- src/components/NotificationViewer/index.tsx
+++ src/components/NotificationViewer/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styled from '@emotion/styled';
 import { Notification } from '@type';
 import Spinner from '@components/Spinner';
@@ -48,6 +49,27 @@ interface NotificationViewePropsType {
 const NotificationViewer = ({
   handleClickCloseViewer,
 }: NotificationViewePropsType) => {
+  const viewerBackGroundRef = useRef(null);
+
+  useEffect(() => {
+    // Viewer 바깥 영역 클릭 시, 뷰어 닫아지도록
+    const handleClickViewerBackGround = (e: MouseEvent) => {
+      if (
+        viewerBackGroundRef.current &&
+        e.target === viewerBackGroundRef.current
+      ) {
+        console.log('배경 클릭 시에만 viewer 닫기');
+        handleClickCloseViewer();
+      }
+    };
+
+    window.addEventListener('click', handleClickViewerBackGround);
+
+    return () => {
+      window.removeEventListener('click', handleClickViewerBackGround);
+    };
+  });
+
   const {
     getNotificationsData,
     isGetNotificationsLoading,
@@ -69,39 +91,51 @@ const NotificationViewer = ({
   if (isGetNotificationsLoading) return <Spinner />;
 
   return (
-    <NotificationViewerContainer>
-      {getNotificationsData?.length === 0 ? (
-        <EmptyNotificationList>알림 목록이 없습니다.</EmptyNotificationList>
-      ) : (
-        <NotificationList>
-          {getNotificationsData!.map((notification) => {
-            return (
-              <NotificationListItem
-                key={notification._id}
-                onClick={handleClickCloseViewer}>
-                {decideNotificationType({ notification })}
-              </NotificationListItem>
-            );
-          })}
-        </NotificationList>
-      )}
-      <NotificationBottomBar>
-        <ReadNotificationButton onClick={handleClickReadNotifications}>
-          모든 알림 확인
-        </ReadNotificationButton>
-      </NotificationBottomBar>
-    </NotificationViewerContainer>
+    <NotificationViewerBackGround ref={viewerBackGroundRef}>
+      <NotificationViewerContainer>
+        {getNotificationsData?.length === 0 ? (
+          <EmptyNotificationList>알림 목록이 없습니다.</EmptyNotificationList>
+        ) : (
+          <NotificationList>
+            {getNotificationsData!.map((notification) => {
+              return (
+                <NotificationListItem
+                  key={notification._id}
+                  onClick={handleClickCloseViewer}>
+                  {decideNotificationType({ notification })}
+                </NotificationListItem>
+              );
+            })}
+          </NotificationList>
+        )}
+        <NotificationBottomBar>
+          <ReadNotificationButton onClick={handleClickReadNotifications}>
+            모든 알림 확인
+          </ReadNotificationButton>
+        </NotificationBottomBar>
+      </NotificationViewerContainer>
+    </NotificationViewerBackGround>
   );
 };
 
 export default NotificationViewer;
 
+const NotificationViewerBackGround = styled.div`
+  background-color: rgba(0, 0, 0, 0);
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+
 const NotificationViewerContainer = styled.div`
-  position: absolute; // TODO: 알림 버튼을 relative
-  top: 120px; // TODO: 알림 버튼 바로 밑에 붙도록
-  right: 0;
-  z-index: 2; // TODO: 나중에 z-index 기준 컴포넌트 별로 설정 필요
-  height: 600px;
+  position: absolute; // TODO: NotificationViewerBackGround 바로 밑에 붙도록 ?
+  top: 140px;
+  right: 100px;
+  z-index: 100; // TODO: 나중에 z-index 기준 컴포넌트 별로 설정 필요
+  height: 300px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -111,7 +145,10 @@ const NotificationViewerContainer = styled.div`
 
 const EmptyNotificationList = styled.div``;
 
-const NotificationList = styled.ul``;
+const NotificationList = styled.ul`
+  height: 250px;
+  overflow-y: scroll;
+`;
 
 const NotificationListItem = styled.li`
   display: flex;
@@ -122,6 +159,7 @@ const NotificationListItem = styled.li`
 `;
 
 const NotificationBottomBar = styled.div`
+  height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
