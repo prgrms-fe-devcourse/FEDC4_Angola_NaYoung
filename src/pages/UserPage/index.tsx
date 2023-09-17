755016f95435c1e04865f333d47646e708f99a5d
JiYoon <angella990825@gmail.com>
2023-09-17 20:16:42 +0900

    refactor: #76 - 상수 작업 및 ui 수정

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -8,9 +8,15 @@ import { authInfoState } from '@atoms/index';
 import { calculateLevel, getUserLevelInfo } from '@utils/calculateUserLevel';
 import UserInfo from './UserInfo';
 
-const UserPage = ({ userId = '' }: { userId?: string }) => {
+const NO_POSTS_LIST_TITLE = '작성한 글이 없습니다.';
+const POSTS_LIST_TITLE = '작성한 포스트';
+
+interface UserPageProps {
+  userId: string;
+}
+
+const UserPage = ({ userId = '' }: UserPageProps) => {
   const auth = useRecoilValue(authInfoState);
-  const myId = auth?.userId;
   const { userData, isUserLoading } = useFetchUser(userId);
   const { userPostsData, isUserPostsLoading } = useFetchUserPosts(userId);
 
@@ -19,7 +25,7 @@ const UserPage = ({ userId = '' }: { userId?: string }) => {
   }
 
   return (
-    <div>
+    <UserPageWrapper>
       {userData && (
         <UserInfo
           userId={userId}
@@ -29,37 +35,85 @@ const UserPage = ({ userId = '' }: { userId?: string }) => {
           followers={userData.followers?.length}
           following={userData.following?.length}
           followerId={
-            userData.followers.find((follower) => follower.follower === myId)
-              ?._id
+            userData.followers.find(
+              (follower) => follower.follower === auth?.userId,
+            )?._id
           }
           userLevel={calculateLevel(userData)}
           userColor={getUserLevelInfo(calculateLevel(userData)).userColor}
           userEmoji={getUserLevelInfo(calculateLevel(userData)).userEmoji}
         />
       )}
-      <ul>
-        {userPostsData?.length === 0 ? (
-          <NoPostMessage>작성한 글이 없습니다.</NoPostMessage>
-        ) : (
-          userPostsData?.map((post) => (
+      <PostsListContainer>
+        <PostsListUl>
+          {/* TODO: 작성한 글이 없을 때는 색상 변경? (레드) */}
+          <PostsListTitle>
+            {userPostsData?.length === 0
+              ? NO_POSTS_LIST_TITLE
+              : POSTS_LIST_TITLE}
+          </PostsListTitle>
+          {/* {userPostsData?.length === 0 ? (
+            <NoPostsListTitle>{NO_POSTS_MESSAGE}</NoPostsListTitle>
+          ) : (
+            <PostsListTitle>{POSTS_LIST_TITLE}</PostsListTitle>
+          )} */}
+          {userPostsData?.map((post) => (
             <PostListItem
               key={post._id}
               id={post._id}
               title={post.title}
             />
-          ))
-        )}
-      </ul>
-    </div>
+          ))}
+        </PostsListUl>
+      </PostsListContainer>
+    </UserPageWrapper>
   );
 };
 
 export default UserPage;
 
-const NoPostMessage = styled.div`
+const UserPageWrapper = styled.div`
+  box-sizing: border-box;
   display: flex;
-  margin-top: 30px;
-  width: 80%;
+  padding: 32px 40px;
+  flex-direction: column;
+  align-items: center;
+  gap: 60px;
+  flex: 1 0 0;
+  align-self: stretch;
+`;
+
+const PostsListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 24px;
+  align-self: stretch;
 `;
+
+const PostsListUl = styled.ul`
+  width: 100%;
+`;
+
+const PostsListTitle = styled.div`
+  color: var(--dark, #9a9a9a);
+  text-align: center;
+  /* title */
+  font-family: Mabinogi_Classic;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 100%; /* 28px */
+`;
+
+// const NoPostsListTitle = styled.div`
+//   color: var(--dark, red);
+//   text-align: center;
+//   /* title */
+//   font-family: Mabinogi_Classic;
+//   font-size: 28px;
+//   font-style: normal;
+//   font-weight: 400;
+//   line-height: 100%; /* 28px */
+// `;
