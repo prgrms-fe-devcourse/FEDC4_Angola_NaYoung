c2dbcfe2fce415e9f32fef7a28c0aadc3a4c237f
박민우 <alsd20177@gmail.com>
2023-09-18 23:41:04 +0900

    style: #91 - LinkButton 기본 style 수정 및 각 컴포넌트에서 사용

--- src/components/NavBar/MenuBar/NonAuthMenuBar.tsx
+++ src/components/NavBar/MenuBar/NonAuthMenuBar.tsx
@@ -1,12 +1,19 @@
+import styled from '@emotion/styled';
 import LinkButton from '@components/LinkButton';
 
 const NonAuthMenuBar = () => {
   return (
     <>
-      <LinkButton to="/login">로그인</LinkButton>
-      <LinkButton to="/signup">회원가입</LinkButton>
+      <RightMenuLinkButton to="/login">로그인</RightMenuLinkButton>
+      <RightMenuLinkButton to="/signup">회원가입</RightMenuLinkButton>
     </>
   );
 };
 
 export default NonAuthMenuBar;
+
+const RightMenuLinkButton = styled(LinkButton)`
+  width: auto;
+  height: 56px;
+  border-radius: 44px;
+`;
