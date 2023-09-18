73fc8d2e430c75a3640fa2fa1dc83ff8e7839682
JiYoon <angella990825@gmail.com>
2023-09-18 16:57:40 +0900

    chore: #32 - 쉼표 제거

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Global styles={{ ...resetStyles, ...commonStyles }} />,
+    <Global styles={{ ...resetStyles, ...commonStyles }} />
     <App />
   </React.StrictMode>,
 );
