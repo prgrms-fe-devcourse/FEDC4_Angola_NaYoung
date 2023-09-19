63897d6952138edde376c5c0d65ce6f1d27d2556
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-09-19 18:11:08 +0900

    chore: #108 - 사용하지 않는 유틸 함수 삭제

--- src/components/Header/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { getPathname } from '@utils';
-import { TITLE } from '../constants';
-
-export const getTitle = (pathname: string) => {
-  return (
-    pathname.startsWith(`/${getPathname(1)}`) && TITLE[`/${getPathname(1)}`]
-  );
-};
