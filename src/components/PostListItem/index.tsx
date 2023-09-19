c18b725b7a446a92587be4ff1671c6a1dba4ebd9
Yeongjun Kim <39931980+dudwns@users.noreply.github.com>
2023-09-19 17:25:56 +0900

    style: #87 - hover 적용 및 post 삭제 버튼 CSS 추가

--- src/components/PostListItem/index.tsx
+++ src/components/PostListItem/index.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import { MORE_LINK_BUTTON_STYLES } from '@styles';
 import { useRecoilValue } from 'recoil';
 import Icon from '@components/Icon';
+import Image from '@components/Image';
 import LinkButton from '@components/LinkButton';
 import Modal from '@components/Modal';
 import { useFetchDeletePost, useFetchUserPosts } from '@apis/post';
@@ -54,32 +55,45 @@ const PostListItem = ({
 
   return (
     <ListItemContainer>
-      <ProfileImage
+      <Image
         src={
           image
             ? image
             : 'https://hips.hearstapps.com/hmg-prod/images/russian-blue-royalty-free-image-1658451809.jpg?crop=0.667xw:1.00xh;0.128xw,0&resize=980:*'
         }
-        alt="프로필"></ProfileImage>
+        alt="프로필"
+        size={60}
+        style={{ margin: '0 20px' }}></Image>
       <TitleContainer>
         <Title>{postTitle}</Title>
       </TitleContainer>
-      <LikesAndComments>
-        <div>♥️ {likes}</div>
-        <div>💬 {comments}</div>
-      </LikesAndComments>
-      <More>
+      {canDeletePost ? (
+        <DeleteButton onClick={handleOpenModal}>
+          <Icon name="trash" />
+        </DeleteButton>
+      ) : (
+        <LikesAndComments>
+          <div>
+            <Icon name="heart" /> {likes}
+          </div>
+          <div>
+            <Icon name="comment" /> {comments}
+          </div>
+        </LikesAndComments>
+      )}
+
+      <More className="more">
         <LinkButton
           to={`/post/${id}`}
           style={MORE_LINK_BUTTON_STYLES}>
           More
         </LinkButton>
       </More>
-      {canDeletePost ? (
+      {/* {canDeletePost ? (
         <button onClick={handleOpenModal}>
           <Icon name="trash" />
         </button>
-      ) : null}
+      ) : null} */}
       {toggleModal && (
         <Modal
           onClose={handleCloseModal}
@@ -103,17 +117,12 @@ const ListItemContainer = styled.li`
   width: 100%;
   border-radius: 24px;
   border: ${ANGOLA_STYLES.border.default};
-  background: #fff;
-  box-shadow: 0px 6px 0px 0px #404040;
-`;
-
-const ProfileImage = styled.img`
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  border: ${ANGOLA_STYLES.border.default};
-  margin: 0px 20px;
-  background: #9a9a9a;
+  background: ${ANGOLA_STYLES.color.white};
+  box-shadow: ${ANGOLA_STYLES.shadow.button.default};
+  overflow: hidden;
+  &:has(.more:hover) {
+    box-shadow: ${ANGOLA_STYLES.shadow.button.hover};
+  }
 `;
 
 const TitleContainer = styled.div`
@@ -127,7 +136,7 @@ const TitleContainer = styled.div`
 `;
 
 const Title = styled.div`
-  color: #404040;
+  color: ${ANGOLA_STYLES.color.text};
   font-size: ${ANGOLA_STYLES.textSize.titleSm};
 `;
 
@@ -136,6 +145,22 @@ const LikesAndComments = styled.div`
   flex-direction: column;
   gap: 12px;
   font-size: ${ANGOLA_STYLES.textSize.title};
+`;
+
+const DeleteButton = styled.div`
+  display: flex;
+  width: 40px;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 44px;
+  border: ${ANGOLA_STYLES.border.default};
+  background: ${ANGOLA_STYLES.color.white};
+  box-shadow: ${ANGOLA_STYLES.shadow.buttonSm.default};
+  cursor: pointer;
+  &:hover {
+    box-shadow: ${ANGOLA_STYLES.shadow.buttonSm.hover};
+  }
 `;
 
 const More = styled.div`
