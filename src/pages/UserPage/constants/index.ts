727a330b17e161e573dae032d45129117ee84e0e
JiYoon <angella990825@gmail.com>
2023-09-20 14:37:09 +0900

    chore: #76 - constants 공통 폴더로 이동

--- src/pages/UserPage/constants/index.ts
+++ src/pages/UserPage/constants/index.ts
@@ -1,21 +1,4 @@
-export const USER_PROFILE_IMAGE = {
-  ALT: '프로필 이미지',
-  DEFAULT_SRC: '/public/images/temp.png',
-};
-
-export const USER_INFO = {
-  LEVEL: 'Level : ',
-  FOLLOWER: '팔로워 : ',
-  FOLLOWING: '팔로잉 : ',
-  GET_LIKES: '받은 좋아요 : ',
-};
-
 export const FOLLOW_MESSAGE = {
   FOLLOW: '팔로우 하기',
   UN_FOLLOW: '언팔로우 하기',
-};
-
-export const USER_POSTS_TITLE = {
-  NO_POSTS: '작성한 포스트가 없습니다.',
-  POSTS: '작성한 포스트',
 };
