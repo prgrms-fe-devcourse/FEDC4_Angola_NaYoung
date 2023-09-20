a9c5da4430a1446b2158fe7d24cb5bc39fad7ecb
JiYoon <angella990825@gmail.com>
2023-09-21 01:25:48 +0900

    feat: #77 - 비밀번호 변경 유효성 검사 로직 추가

--- src/pages/MyPage/hooks/useUpdatePassWord.ts
+++ src/pages/MyPage/hooks/useUpdatePassWord.ts
@@ -4,9 +4,15 @@ import { checkPassWordPattern } from '@utils/userAuthentication';
 
 const useUpdatePassWord = () => {
   const { updatePasswordMutate, updatePasswordData } = useFetchUpdatePassword();
-  const [newPassWord, setNewPassWord] = useState(updatePasswordData.password);
+  const [newPassWord, setNewPassWord] = useState(
+    updatePasswordData.password as string,
+  );
   const [confirmNewPassWord, setConfirmNewPassWord] = useState('');
   const [isEditingPassWord, setIsEditingPassWord] = useState(false);
+  const [invalidPasswordMsg, setInvalidPasswordMsg] = useState('');
+  const [invalidPasswordConfirmMsg, setInvalidPasswordConfirmMsg] =
+    useState('');
+  const [validPasswordConfirmMsg, setValidPasswordConfirmMsg] = useState('');
 
   const resetPassWordFields = () => {
     setNewPassWord('');
@@ -27,11 +33,45 @@ const useUpdatePassWord = () => {
   };
 
   const handleChangePassWord = (e: ChangeEvent<HTMLInputElement>) => {
+    const { passwordMsg, isValidPassword } = checkPassWordPattern({
+      newPassWord: e.target.value,
+      confirmNewPassWord,
+    });
     setNewPassWord(e.target.value);
+
+    if (!e.target.value) {
+      setInvalidPasswordMsg('비밀번호를 입력해주세요.');
+    } else if (!isValidPassword) {
+      setInvalidPasswordMsg(passwordMsg);
+    } else {
+      setInvalidPasswordMsg('');
+    }
   };
 
   const handleChangeConfirmPassWord = (e: ChangeEvent<HTMLInputElement>) => {
+    const { passwordConfirmMsg, isValidPasswordConfirm } = checkPassWordPattern(
+      {
+        newPassWord,
+        confirmNewPassWord: e.target.value,
+      },
+    );
     setConfirmNewPassWord(e.target.value);
+    setValidPasswordConfirmMsg('');
+
+    if (!isValidPasswordConfirm) {
+      setInvalidPasswordConfirmMsg(passwordConfirmMsg);
+    } else {
+      setValidPasswordConfirmMsg(passwordConfirmMsg);
+      setInvalidPasswordConfirmMsg('');
+    }
+  };
+
+  const handleAcceptPassWordButton = () => {
+    if (!validPasswordConfirmMsg || invalidPasswordMsg) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return {
@@ -41,6 +81,10 @@ const useUpdatePassWord = () => {
     handleClickUpdatePassWord,
     confirmNewPassWord,
     handleChangeConfirmPassWord,
+    invalidPasswordMsg,
+    invalidPasswordConfirmMsg,
+    validPasswordConfirmMsg,
+    handleAcceptPassWordButton,
   };
 };
 
