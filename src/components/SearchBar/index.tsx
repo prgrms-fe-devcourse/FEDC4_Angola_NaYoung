ed7da1f804760a7e262e9eeab7d809241a9a4e7d
박민우 <alsd20177@gmail.com>
2023-09-21 21:55:19 +0900

    style: #138 - SearchBar 검색창 반응형 css 구현

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -28,7 +28,7 @@ const SearchBar = () => {
 
     if (keyword.length) {
       addKeywordToQueryString({ keyword: keyword.trim() });
-      setKeyword(keyword.trim());
+      setKeyword('');
     } else {
       removeKeywordFromQueryString();
     }
@@ -55,7 +55,6 @@ const SearchBar = () => {
         onChange={handleChangeKeyword}></StyledInput>
 
       <ClearTextButton type="reset">
-        {' '}
         <Icon
           name="close"
           size={'30'}
@@ -70,8 +69,6 @@ export default SearchBar;
 const StyledForm = styled.form`
   box-sizing: border-box;
   position: relative;
-  max-width: 600px;
-  width: 600px;
   height: 60px;
   display: flex;
   padding: 0px 20px;
@@ -80,9 +77,13 @@ const StyledForm = styled.form`
   border: ${ANGOLA_STYLES.border.default};
   background: ${ANGOLA_STYLES.color.white};
   box-shadow: ${ANGOLA_STYLES.shadow.input.default};
-  
-  &:focus-within{
+
+  &:focus-within {
     box-shadow: ${ANGOLA_STYLES.shadow.input.focus};
+  }
+
+  @media (max-width: 660px) {
+    display: none;
   }
 `;
 
@@ -101,7 +102,11 @@ const StyledInput = styled.input`
   margin: 0 0 0 12px;
   padding: 0;
   height: 30px;
-  width: 300px; // calc하기
+  width: calc((100vw - 514px) * 0.65);
+
+  @media (max-width: 780px) {
+    width: calc((100vw - 514px) * 0.4);
+  }
 `;
 
 const ClearTextButton = styled.button`
