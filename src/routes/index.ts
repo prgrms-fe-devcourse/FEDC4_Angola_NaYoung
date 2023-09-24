4b331bd134575c8febbfc258b9daa89056e17135
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-25 02:45:14 +0900

    feat: #193 - auth 리다이렉트 route 에서 관리하는 것으로 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -18,41 +18,71 @@ export const routes: {
   name: string;
   title: string;
   component: RouteComponent<SearchParams & Params>;
+  authRequired: boolean;
 }[] = [
   {
     path: '*',
     name: '404',
     title: '알 수 없는 페이지',
     component: NotFoundPage,
+    authRequired: false,
   },
-  { path: '/', name: 'home', title: 'ANGOLA', component: HomePage },
+  {
+    path: '/',
+    name: 'home',
+    title: 'ANGOLA',
+    component: HomePage,
+    authRequired: false,
+  },
   {
     path: '/search/:target',
     name: 'search',
     title: '에 대한 검색 결과',
     component: SearchPage,
+    authRequired: false,
   },
   {
     path: '/signup',
     name: 'signUp',
     title: '회원 가입',
     component: SignUpPage,
+    authRequired: false,
   },
-  { path: '/login', name: 'login', title: '로그인', component: LoginPage },
+  {
+    path: '/login',
+    name: 'login',
+    title: '로그인',
+    component: LoginPage,
+    authRequired: false,
+  },
   {
     path: '/user/:userId',
     name: 'user',
     title: '유저 정보',
     component: UserPage,
+    authRequired: false,
   },
-  { path: '/post/:postId', name: 'post', title: 'ANGOLA', component: PostPage },
+  {
+    path: '/post/:postId',
+    name: 'post',
+    title: 'ANGOLA',
+    component: PostPage,
+    authRequired: false,
+  },
   {
     path: '/create-post',
     name: 'createPost',
     title: '추가하기',
     component: CreatePostPage,
+    authRequired: true,
   },
-  { path: '/mypage', name: 'myPage', title: '마이 페이지', component: MyPage },
+  {
+    path: '/mypage',
+    name: 'myPage',
+    title: '마이 페이지',
+    component: MyPage,
+    authRequired: true,
+  },
 ];
 
 export const redirects = [
