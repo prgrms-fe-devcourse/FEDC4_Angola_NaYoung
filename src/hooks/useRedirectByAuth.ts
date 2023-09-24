21a577947eae1a313674eb0ebbf172f30272d19b
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2023-09-24 21:32:45 +0900

    feat: #193 - default 주소 추가

--- src/hooks/useRedirectByAuth.ts
+++ src/hooks/useRedirectByAuth.ts
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { authInfoState } from '@store/auth';
 
-export const useRedirectByAuth = (to: string) => {
+export const useRedirectByAuth = (to = '/') => {
   const navigate = useNavigate();
   const auth = useRecoilValue(authInfoState);
 
