43690d2431b66e244fe4e5f8e4a51c8940953242
hayamaster <dnfxmfkdbzos@naver.com>
2023-09-24 14:11:08 +0900

    style: #177 - 댓글 삭제시, 모달 버튼을 컴포넌트의 default값으로 수정

--- src/pages/PostPage/components/CommentDeletionModal.tsx
+++ src/pages/PostPage/components/CommentDeletionModal.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import Button from '@components/Button';
 import Modal from '@components/Modal';
 import { DELETE_COMMENT } from '../constants';
 
@@ -15,24 +14,9 @@ const CommentDeletionModal = ({
   return (
     <Modal
       onClose={() => setIsClickedDeleteBtn(false)}
-      onConfirm={handleClickDeleteCommentModalBtn}
-      footerShow={false}>
+      onConfirm={handleClickDeleteCommentModalBtn}>
       <ModalWrapper onClick={(e) => e.stopPropagation()}>
         <div>{DELETE_COMMENT.TITLE}</div>
-        <ButtonContainer>
-          <Button
-            size="sm"
-            onClick={handleClickDeleteCommentModalBtn}
-            style={{ width: '50px', height: '40px', color: '#F66' }}>
-            {DELETE_COMMENT.YES}
-          </Button>
-          <Button
-            size="sm"
-            onClick={() => setIsClickedDeleteBtn(false)}
-            style={{ width: '90px', height: '40px' }}>
-            {DELETE_COMMENT.NO}
-          </Button>
-        </ButtonContainer>
       </ModalWrapper>
     </Modal>
   );
@@ -44,15 +28,7 @@ const ModalWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 150px;
+  height: 100px;
   justify-content: space-around;
   align-items: center;
-  gap: 50px;
-`;
-
-const ButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
 `;
